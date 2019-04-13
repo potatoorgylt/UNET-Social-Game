3a06fac213597919fedafe6d90a9839d69b8d92c
potatoorgylt <gynejas13@gmail.com>
2019-04-14 01:02:56 +0300

    Diversity update. Hair and skin color switch options

---
+++ Assets/Scripts/ColorPickerReceiver.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ColorPickerReceiver : MonoBehaviour
+{
+    public Renderer[] skinRends;
+    private SkinColorChanger skinColorChanger = new SkinColorChanger();
+
+    public ObjectToColor[] objectsToColor;
+
+    void Start()
+    {
+        InitSkinColor();
+
+        foreach(ObjectToColor objToColor in objectsToColor)
+        {
+            objToColor.picker.onValueChanged.AddListener(color =>
+            {
+                foreach(Renderer rend in objToColor.renderer)
+                    rend.material.color = color;
+                objToColor.Color = color;
+            });
+
+            foreach (Renderer rend in objToColor.renderer)
+                rend.material.color = objToColor.picker.CurrentColor;
+
+            objToColor.picker.CurrentColor = objToColor.Color;
+        }
+    }
+
+    public void InitSkinColor()
+    {
+        for (int i = 0; i < skinRends.Length; i++)
+        {
+            skinColorChanger.AddSkinRenderer(skinRends[i]);
+        }
+    }
+
+    public void ChangeSkinColor(float range)
+    {
+        skinColorChanger.ChangeSkin(range);
+    }
+
+    [System.Serializable]
+    public class ObjectToColor
+    {
+        public new Renderer[] renderer;
+        public ColorPicker picker;
+
+        public Color Color;
+    }
+}
---
+++ Assets/Scripts/SkinColorChanger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SkinColorChanger
+{
+    private List<Renderer> skinRenderer = new List<Renderer>();
+
+    public Renderer GetSkinRenderer(int id)
+    {
+        return skinRenderer[id];
+    }
+    public void SetSkinRenderer(int id, Renderer value)
+    {
+        skinRenderer[id] = value;
+    }
+    public void AddSkinRenderer(Renderer rend)
+    {
+        skinRenderer.Add(rend);
+    }
+
+    private Color startValue = new Color(1f, 1f, 1f, 1f);
+    private Color endValue = new Color(22f/255f, 13f/255f, 0f, 1f);
+    public Color curColor;
+
+    public void ChangeSkin(float range)
+    {
+        curColor.r = Mathf.Lerp(endValue.r, startValue.r, 1f - range);
+        curColor.g = Mathf.Lerp(endValue.g, startValue.g, 1f - range);
+        curColor.b = Mathf.Lerp(endValue.b, startValue.b, 1f - range);
+
+        for (int i = 0; i < skinRenderer.Count; i++)
+        {
+            skinRenderer[i].materials[2].color = curColor;
+        }
+    }
+}
