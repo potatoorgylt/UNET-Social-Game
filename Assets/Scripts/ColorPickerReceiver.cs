f2cce1d6404f03f49ba866ec34b2b2db2aeab47d
potatoorgylt <gynejas13@gmail.com>
2019-04-15 02:43:37 +0300

    Diversity update 2, sweater and pants customization, animation transitions

--- Assets/Scripts/ColorPickerReceiver.cs
+++ Assets/Scripts/ColorPickerReceiver.cs
@@ -18,12 +18,12 @@ public class ColorPickerReceiver : MonoBehaviour
             objToColor.picker.onValueChanged.AddListener(color =>
             {
                 foreach(Renderer rend in objToColor.renderer)
-                    rend.material.color = color;
+                    rend.materials[objToColor.matId].color = color;
                 objToColor.Color = color;
             });
 
             foreach (Renderer rend in objToColor.renderer)
-                rend.material.color = objToColor.picker.CurrentColor;
+                rend.materials[objToColor.matId].color = objToColor.picker.CurrentColor;
 
             objToColor.picker.CurrentColor = objToColor.Color;
         }
@@ -45,6 +45,7 @@ public class ColorPickerReceiver : MonoBehaviour
     [System.Serializable]
     public class ObjectToColor
     {
+        public int matId = 0;
         public new Renderer[] renderer;
         public ColorPicker picker;
 
